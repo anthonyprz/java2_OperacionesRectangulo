53a5e1c1a5a5c657b15c54a4759e9aa66c818b2c
zubiri <zubiri@Ubuntu-12-04.(none)>
2014-09-30 08:45:12 +0200

    listo

---
+++ src/Rectangulo.java
@@ -0,0 +1,50 @@
+import java.lang.Math;
+
+public class Rectangulo {
+
+//	public double altura;
+//	public double base;
+//	public double perimetro;
+//	public double area;
+
+  	   
+	    private double base = 0;
+	    private double altura = 0; 
+	
+
+ 	public void setAltura (double alt) {
+		this.altura = alt;
+	}
+	
+	public void setBase (double bas) {
+		this.base = bas;
+	}
+
+	public double getAltura ()   {
+
+		
+        	return this.altura;
+		
+	} 
+
+	public double getBase ()   {
+
+		
+        	return this.base;
+		
+	}
+	public double perimetro ()   {
+
+
+		double perimetro = 2*(base+altura); 
+        	return perimetro;
+
+		} 
+	public double area ()   {
+
+		double area = base*altura;
+        	return area;
+		
+	} 
+	
+}
